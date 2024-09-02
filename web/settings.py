94baf1898266d69748ded166daa9cb216dcb03fd
c_Door in <egor1@mail.ru>
2024-09-02 20:38:37 +0300

    Fix root folder for azure storage file

--- web/settings.py
+++ web/settings.py
@@ -156,8 +156,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
-STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
+DEFAULT_FILE_STORAGE = 'web.azure_storage.AzureMediaStorage'
+STATICFILES_STORAGE = 'web.azure_storage.AzureStaticStorage'
 
 AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
 AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
